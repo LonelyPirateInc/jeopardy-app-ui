2d5af4a5b9350eec0add75494f8d29bcddf3bf81
anto0084 <anto0084@algonquinlive.com>
2018-11-07 20:50:18 -0500

    added socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './data/socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jeopardy-app-ui';
+
+
+  constructor(
+    private socketService: SocketService,
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.socketService.setupConnection(socket => console.log(socket));
+  }
 }
---
+++ src/app/data/socket.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscriber, Subject } from 'rxjs';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: SocketIOClient.Socket;
+  public socketConnectionSubject = new Subject<boolean>();
+  
+  constructor() { }
+
+
+  setupConnection(socketConfig: any): SocketIOClient.Socket {
+    const configuration = socketConfig;
+    // const token: string = this.localStorageService.get<string>(AuthenticationService.TokenKey);
+    // configuration['query'] = `auth_token=${token}`;
+
+    return io('http://127.0.0.1:3306');
+  }
+
+
+}
