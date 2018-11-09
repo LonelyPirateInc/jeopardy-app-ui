0cc39c6236f5d1c10d711edbcfd0cd3cf5dec109
Min <minkim.dev@gmail.com>
2018-11-09 10:25:40 -0500

    adding socket implementation

--- src/app/data/socket.service.ts
+++ src/app/data/socket.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { Observable, Subscriber, Subject, config } from 'rxjs';
 import * as io from 'socket.io-client';
@@ -6,15 +7,28 @@ import * as io from 'socket.io-client';
   providedIn: 'root'
 })
 export class SocketService {
-  // private socket: SocketIOClient.Socket;
+  public socket: SocketIOClient.Socket;
   public socketConnectionSubject = new Subject<boolean>();
   constructor() { }
-  setupConnection(socketConfig?: any): SocketIOClient.Socket {
+  setupConnection(socketConfig?: any): void {
     const configuration = socketConfig;
-    // const token: string = this.localStorageService.get<string>(AuthenticationService.TokenKey);
-    // configuration['query'] = `auth_token=${token}`;
-    return io('http://127.0.0.1:8083', configuration);
+    this.socket = io('http://127.0.0.1:8083', { multiplex: true });
+    this.socket.emit('events', { test: 'testing' });
+    this.socket.on('events', (data) => {
+      console.log('connecting...');
+      console.log(data);
+    });
+    this.socket.on('questionSelected', (data) => {
+      console.log(data);
+    });
+
+    this.socket.on('events', (data) => {
+      console.log(data);
+    });
   }
 
-
+  sendQuestionSelectEvent(question: any): void {
+    this.socket.emit('events', { test: 'testing' });
+    this.socket.emit('questionSelected', question);
+  }
 }
