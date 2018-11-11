cbdc09ab995ae466f7a466aef6bf7cc02f45c55b
Min <minkim.dev@gmail.com>
2018-11-11 09:47:56 -0500

    routing fix

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from 'src/app/data/game/game.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, mergeMap,filter } from 'rxjs/operators';
 import { QuestionService } from 'src/app/data/question/question.service';
 
@@ -8,13 +8,13 @@ import { of, forkJoin } from 'rxjs';
 import * as sortyBy from 'lodash/sortBy';
 import * as groupBy from 'lodash/groupBy';
 import * as isEmpty from 'lodash/isEmpty';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { SocketService } from 'src/app/data/socket.service';
 
 @Component({
-  selector: 'app-game-page',
-  templateUrl: './game-page.component.html',
-  styleUrls: ['./game-page.component.scss']
+  selector: "app-game-page",
+  templateUrl: "./game-page.component.html",
+  styleUrls: ["./game-page.component.scss"]
 })
 export class GamePageComponent implements OnInit {
   questions: any;
@@ -27,15 +27,43 @@ export class GamePageComponent implements OnInit {
   showGamePlay: boolean;
   selectedQuestion: any;
   isAllInAvailable: boolean;
+  navigationSubscription: Subscription;
 
   constructor(
     private gameService: GameService,
     private questionService: QuestionService,
     private router: Router,
-    private socketService: SocketService,
-  ) { }
+    private socketService: SocketService
+  ) {}
 
   ngOnInit() {
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      if (e instanceof NavigationEnd) {
+        forkJoin(
+          this.questionService.getQuestionCategories(),
+          this.gameService.getRecentGame()
+        ).subscribe((data: [any, any]) => {
+          const [categories, game] = data;
+          this.questions = game.questions;
+          this.sortQuestionsByCategory(this.questions, categories);
+
+          const categoriesGrouped = groupBy(
+            categories,
+            category => category.isAllIn
+          );
+          this.categories = categoriesGrouped[false];
+          this.allInCategory = categoriesGrouped[true];
+          console.log(this.allInCategory);
+          this.isAllInAvailable = !isEmpty(
+            this.allInCategory[0].questions.filter(question => !question.isActive)
+          );
+          this.socketService.socket.emit("gameStart", game);
+          this.existsGame = true;
+          this.game = game;
+        });
+      }
+    });
+
     forkJoin(
       this.questionService.getQuestionCategories(),
       this.gameService.getRecentGame()
@@ -44,20 +72,30 @@ export class GamePageComponent implements OnInit {
       this.questions = game.questions;
       this.sortQuestionsByCategory(this.questions, categories);
 
-      const categoriesGrouped = groupBy(categories, category => category.isAllIn);
+      const categoriesGrouped = groupBy(
+        categories,
+        category => category.isAllIn
+      );
       this.categories = categoriesGrouped[false];
       this.allInCategory = categoriesGrouped[true];
       console.log(this.allInCategory);
-      this.isAllInAvailable = !isEmpty(this.allInCategory[0].questions.filter(question => !question.isActive));
-      this.socketService.socket.emit('gameStart', game);
+      this.isAllInAvailable = !isEmpty(
+        this.allInCategory[0].questions.filter(question => !question.isActive)
+      );
+      this.socketService.socket.emit("gameStart", game);
       this.existsGame = true;
       this.game = game;
     });
   }
 
   private sortQuestionsByCategory(questions: any, categories: any): void {
-    const sortedQuestions = groupBy(questions, question => question.category.id);
-    categories.forEach(category => category.questions = sortedQuestions[category.id]);
+    const sortedQuestions = groupBy(
+      questions,
+      question => question.category.id
+    );
+    categories.forEach(
+      category => (category.questions = sortedQuestions[category.id])
+    );
   }
 
   public createGame(name: string): void {
@@ -68,12 +106,15 @@ export class GamePageComponent implements OnInit {
       const [categories, game] = data;
       this.questions = game.questions;
       this.sortQuestionsByCategory(this.questions, categories);
-      const categoriesGrouped = groupBy(categories, category => category.isAllIn);
+      const categoriesGrouped = groupBy(
+        categories,
+        category => category.isAllIn
+      );
       this.categories = categoriesGrouped[false];
       this.allInCategory = categoriesGrouped[true];
       this.existsGame = true;
       this.game = game;
-      this.socketService.socket.emit('gameStart', game);
+      this.socketService.socket.emit("gameStart", game);
       this.handleModalCancel();
     });
   }
@@ -93,12 +134,12 @@ export class GamePageComponent implements OnInit {
   }
 
   public showQuestion(question: any): void {
-    const playUrl = 'host/play/';
+    const playUrl = "host/play/";
     this.router.navigate([playUrl, question.id]);
   }
 
   public showAllIn(): void {
-    const playUrl = 'host/play/';
+    const playUrl = "host/play/";
     const question = this.allInCategory[0].questions[0];
     this.router.navigate([playUrl, question.id]);
   }
